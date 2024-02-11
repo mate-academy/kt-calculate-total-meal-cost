b42b68b6397bf43fdc8fd1f459d48b4696034dd3
Nikita <nsalogub96@gmail.com>
2024-02-11 15:55:18 +0200

    done

--- src/main/kotlin/MealCostCalculator.kt
+++ src/main/kotlin/MealCostCalculator.kt
@@ -1,1 +1,11 @@
-// create a function and implement it
+fun calculateTotalMealCost(
+    baseCost: Double,
+    taxRate: Double,
+    tipPercentage: Double,
+    discount: Double = 0.0
+): Double {
+    fun applyTax() = taxRate * baseCost
+    fun applyTip() = (baseCost + applyTax()) * tipPercentage
+    fun applyDiscount() = baseCost * discount / 100
+    return baseCost + applyTax() + applyTip() - applyDiscount()
+}
