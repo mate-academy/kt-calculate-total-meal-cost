1390f869c154dcad268bb08ea57be0dbc22d9736
Danil Ponomarenko <danil.ponomvrenko@gmail.com>
2024-03-06 13:56:52 +0200

    Practiced local function & lambdas syntax

--- src/main/kotlin/MealCostCalculator.kt
+++ src/main/kotlin/MealCostCalculator.kt
@@ -1,1 +1,29 @@
 // create a function and implement it
+fun calculateTotalMealCost(
+    baseCost: Double,
+    taxRate: Double,
+    tipPercentage: Double,
+    discount: Double = 0.0
+): Double {
+    var totalCost = baseCost
+
+    fun applyTax(): Double {
+        totalCost += baseCost * taxRate
+        return totalCost
+    }
+
+    fun applyTip(): Double {
+        totalCost += totalCost * tipPercentage
+        return totalCost
+    }
+
+    val applyDiscount: (Double) -> Unit = {
+        if (discount > 0) totalCost -= discount
+    }
+
+    applyTax()
+    applyTip()
+    applyDiscount(discount)
+
+    return totalCost
+}
