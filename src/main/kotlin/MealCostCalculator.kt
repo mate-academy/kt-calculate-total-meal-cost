ff0d6717b46e254ad4b6f699370f11d12eea3570
Volodymyr Ozemblovskyi <Vladimir.ozem.ps@gmail.com>
2024-03-13 19:23:41 +0200

    solved task from Readme file: - increased jvmToolchain version up to 17 from 8 - created and implemented calculateTotalMealCost() fun

--- src/main/kotlin/MealCostCalculator.kt
+++ src/main/kotlin/MealCostCalculator.kt
@@ -1,1 +1,7 @@
 // create a function and implement it
+fun calculateTotalMealCost (baseCost: Double, taxRate: Double, tipPercentage: Double, discount: Double = 0.0): Double {
+    fun applyTax() = baseCost + baseCost * taxRate
+    fun applyTip(tax: Double) = tax + tax * tipPercentage
+    fun applyDiscount(tip: Double) = tip - discount
+    return applyDiscount(applyTip(applyTax()))
+}
