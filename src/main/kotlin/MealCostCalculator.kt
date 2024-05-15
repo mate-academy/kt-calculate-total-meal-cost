7c891aae4f042ca900fc483301103e0517aec477
Danylo Borysenko <borysenkodanyl@gmail.com>
2024-05-15 17:02:43 +0300

    first commit

--- src/main/kotlin/MealCostCalculator.kt
+++ src/main/kotlin/MealCostCalculator.kt
@@ -1,1 +1,20 @@
-// create a function and implement it
+const val HUNDRED = 100
+
+fun calculateTotalMealCost(
+    baseCost : Double, taxRate : Double,
+    tipPercentage : Double, discount : Double = 0.0) : Double {
+    var result = baseCost
+    fun applyTax() {
+        result += result * taxRate
+    }
+    fun applyTip() {
+        result += result * tipPercentage
+    }
+    fun applyDiscount() {
+        result -= discount
+    }
+    applyTax()
+    applyTip()
+    applyDiscount()
+    return result
+}
