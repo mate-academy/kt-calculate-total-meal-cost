c8cb5d6c0537f10583d1b2294a6372b052962b89
Artem Kostenko <a_kostenco@yahoo.com>
2024-05-22 22:08:10 +0300

    Total meal cost solution

--- src/main/kotlin/MealCostCalculator.kt
+++ src/main/kotlin/MealCostCalculator.kt
@@ -1,1 +1,19 @@
-// create a function and implement it
+fun calculateTotalMealCost(baseCost :Double, taxRate :Double, tipPercentage :Double, discount :Double = 0.0) :Double {
+    fun applyTax(value :Double) :Double {
+        return value + value * taxRate
+    }
+
+    fun applyTip(value :Double) :Double {
+        return value + value * tipPercentage
+    }
+
+    fun applyDiscount(value :Double) :Double {
+        return value - discount
+    }
+
+    var totalMealCost = applyTax(baseCost)
+    totalMealCost = applyTip(totalMealCost)
+    totalMealCost = applyDiscount(totalMealCost)
+
+    return totalMealCost
+}
