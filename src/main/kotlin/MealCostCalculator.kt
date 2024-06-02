f2a9fd6c672dc5814b747d74e4cc0f0b1f09d035
Roman Globa <globaroman@gmail.com>
2024-06-02 21:12:41 +0300

    The solution of the task++

--- src/main/kotlin/MealCostCalculator.kt
+++ src/main/kotlin/MealCostCalculator.kt
@@ -1,20 +1,27 @@
 // create a function and implement it
 const val DISCOUNT_LEVEL: Double = 0.00
+fun main() {
+    val cost = calculateTotalMealCost(100.0, 0.1, 0.15)
+
+    println(cost)
+
+}
+
 fun calculateTotalMealCost(baseCost: Double,
                            taxRate: Double,
                            tipPercentage: Double,
                            discount: Double = DISCOUNT_LEVEL): Double {
-    fun tax(baseCost: Double, taxRate: Double): Double {
+    fun calculateTax(baseCost: Double, taxRate: Double): Double {
         return baseCost * taxRate
     }
 
-    fun tip(baseCost: Double, tipPercentage: Double): Double {
+    fun calculateTip(baseCost: Double, tipPercentage: Double): Double {
         return baseCost * tipPercentage
     }
 
-    val costAfterTaxRate = tax(baseCost, taxRate)
-    val costAfterTipPercentage = tip(baseCost, tipPercentage)
-    val totalCost = baseCost + costAfterTaxRate + costAfterTipPercentage
+    val costAfterTax = baseCost + calculateTax(baseCost, taxRate)
+    val costAfterTip = costAfterTax + calculateTip(baseCost, tipPercentage)
+    val totalCost = costAfterTip
 
     return if (discount > DISCOUNT_LEVEL) totalCost - discount else totalCost
 }
