847efd608b98bbbbb9a7baee74b54330f685e76e
serhiibiria <serhiiburia@gmail.com>
2024-06-21 15:06:52 +0300

    added solution

--- src/main/kotlin/MealCostCalculator.kt
+++ src/main/kotlin/MealCostCalculator.kt
@@ -1,1 +1,53 @@
-// create a function and implement it
+
+fun calculateTotalMealCost(baseCost: Double,
+                           taxRate: Double,
+                           tipPercentage: Double,
+                           discount: Double) : Double {
+    var totalCost: Double = 0.0
+
+    fun applyTax(mealCost: Double,
+                 taxRate: Double) : Double {
+        return baseCost.plus(baseCost.times(taxRate))
+    }
+
+    fun applyTip(mealCost: Double, tipPercentage: Double): Double {
+        return mealCost.plus(mealCost.times(tipPercentage))
+    }
+
+
+    fun applyDiscount(mealCost: Double, discount: Double): Double {
+        return mealCost.minus(discount)
+    }
+
+    totalCost = applyTax(baseCost, taxRate)
+    totalCost = applyTip(totalCost, tipPercentage)
+    if (discount > 0) {
+        totalCost = applyDiscount(totalCost, discount)
+    }
+    return totalCost
+}
+
+fun calculateTotalMealCost(baseCost: Double,
+                           taxRate: Double,
+                           tipPercentage: Double) : Double {
+    var totalCost: Double = 0.0
+
+    fun applyTax(mealCost: Double,
+                 taxRate: Double) : Double {
+        return mealCost.plus(mealCost.times(taxRate))
+    }
+
+    fun applyTip(mealCost: Double, tipPercentage: Double): Double {
+        return mealCost.plus(mealCost.times(tipPercentage))
+    }
+
+    fun applyDiscount(mealCost: Double, discount: Double): Double {
+        return mealCost.minus(discount)
+    }
+
+    totalCost = applyTax(baseCost, taxRate)
+    totalCost = applyTip(totalCost, tipPercentage)
+
+    return totalCost
+}
+
