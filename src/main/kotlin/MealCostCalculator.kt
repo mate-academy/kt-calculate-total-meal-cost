9c7729f1cc06466958418c7f0b2512bc490fd814
Alex Zavodov <96802636+lexzane@users.noreply.github.com>
2024-07-11 04:38:18 +0300

    feat [kotlin]: implement simple cost calculator

--- src/main/kotlin/MealCostCalculator.kt
+++ src/main/kotlin/MealCostCalculator.kt
@@ -1,1 +1,17 @@
-// create a function and implement it
+fun calculateTotalMealCost(
+    baseCost: Double,
+    taxRate: Double,
+    tipPercentage: Double,
+    discount: Double = 0.0,
+): Double {
+    fun applyTax(baseCost: Double, taxRate: Double) = baseCost + baseCost * taxRate
+
+    fun applyTip(costAfterTax: Double, tipPercentage: Double) = costAfterTax + costAfterTax * tipPercentage
+
+    fun applyDiscount(costAfterTaxAndTip: Double, discount: Double) = costAfterTaxAndTip - discount
+
+    val costAfterTax = applyTax(baseCost, taxRate)
+    val costAfterTaxAndTip = applyTip(costAfterTax, tipPercentage)
+
+    return applyDiscount(costAfterTaxAndTip, discount)
+}
