78bc078f61121a06a53d16578e420e63132a1b46
Max <mpbakunov@gmail.com>
2024-07-26 19:15:05 +0200

    Bugs fixing #01

--- src/main/kotlin/MealCostCalculator.kt
+++ src/main/kotlin/MealCostCalculator.kt
@@ -1,15 +1,16 @@
 // create a function and implement it
-fun main() {
-    val baseCost = 100.0
-    val taxRate = 0.1 // 10%
-    val tipPercentage = 0.15 // 15%
-    val discount = 10.0 // $10 discount for special occasions
+const val BASE_COST = 100.0
+const val TAX_RATE = 0.1 // 10%
+const val TIP_PERCENTAGE = 0.15 // 15%
+const val DISCOUNT = 10.0 // $10 discount for special occasions
 
-    val totalMealCost = calculateTotalMealCost(baseCost, taxRate, tipPercentage, discount)
+fun main() {
+    val totalMealCost = calculateTotalMealCost(BASE_COST, TAX_RATE, TIP_PERCENTAGE, DISCOUNT)
     println("Total meal cost: $totalMealCost")
 }
 
-fun calculateTotalMealCost(baseCost : Double, taxRate : Double, tipPercentage : Double, discount : Double = 0.0) : Double {
+fun calculateTotalMealCost(baseCost : Double, taxRate : Double
+                           , tipPercentage : Double, discount : Double = 0.0) : Double {
     fun applyTax(baseCost : Double, taxRate : Double) = baseCost * (1 + taxRate)
     fun tipPercentage(baseCost : Double, tipPercentage : Double) = baseCost * (1 + tipPercentage)
     fun applyDiscount(baseCost : Double, discount : Double) = baseCost - discount
