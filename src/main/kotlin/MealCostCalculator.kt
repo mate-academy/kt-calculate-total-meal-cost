f64e7f11f6e1be2f73e4cf8fd95d895cc36e15fb
Viktoriia Ihnatiuk <viktoriia.ihnatiuk@techtellent.io>
2024-08-26 16:42:08 +0300

    kt-calculate-meal-cost task has been implemented

--- src/main/kotlin/MealCostCalculator.kt
+++ src/main/kotlin/MealCostCalculator.kt
@@ -1,1 +1,18 @@
-// create a function and implement it
+
+
+fun calculateTotalMealCost(baseCost: Double, taxRate: Double, tipPercentage: Double, discount: Double = 0.0) : Double{
+    var finalCost = 0.0
+    fun applyTax() {
+       finalCost = baseCost + baseCost * taxRate
+    }
+    fun applyTip() {
+        finalCost += finalCost * tipPercentage
+    }
+    fun applyDiscount() {
+        finalCost -= discount
+    }
+    applyTax()
+    applyTip()
+    applyDiscount()
+    return finalCost
+}
