53a2f2a6c649bbb75966967af7f9719cf4d07ed1
Kateryna Mykhailovska <112807544+kateryna-mykh@users.noreply.github.com>
2025-03-09 22:41:39 +0200

    Update MealCostCalculator.kt

--- src/main/kotlin/MealCostCalculator.kt
+++ src/main/kotlin/MealCostCalculator.kt
@@ -1,1 +1,12 @@
 // create a function and implement it
+fun calculateTotalMealCost(baseCost: Double, taxRate: Double, tipPercentage: Double, discount: Double = 0.0): String {
+    fun applyTax(finalCost: Double) = finalCost + (finalCost * taxRate)
+    fun applyTip(finalCost: Double) = finalCost + (finalCost * tipPercentage)
+    fun applyDiscount(finalCost: Double) = finalCost - discount
+
+    var finalCost = baseCost
+    finalCost = applyTax(finalCost)
+    finalCost = applyTip(finalCost)
+    finalCost = applyDiscount(finalCost)
+    return "Total meal cost: $finalCost."
+}
